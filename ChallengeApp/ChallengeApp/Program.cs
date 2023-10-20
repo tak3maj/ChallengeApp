1997c6e8ea78f1c7989560372399a8d0433a9efa
tak3maj <tak3maj@wp.pl>
2023-10-20 23:44:34 +0200

    Zadanie dzień 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Zosia";
+var sex = "woman";
+var age = 33;
+
+if (sex == "woman")
+{
+    if (name == "Ewa")
+    {
+        if (age == 33)
+        {
+            Console.WriteLine("Ewa, lat 33");
+        }
+        else if (age < 30)
+        {
+            Console.WriteLine("Kobieta poniżej 30 lat");
+        }
+        else
+        {
+            Console.WriteLine("Kobieta powyżej 30 lat");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Na pewno nie jestem Ewą");
+    }
+}
+else if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
