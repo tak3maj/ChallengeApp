fcfb9e78faab78d551779c4dbb01cc9f46a0622a
tak3maj <tak3maj@wp.pl>
2023-10-26 19:52:58 +0200

    Dzień 6

---
+++ Klasy/Klasy/Program.cs
@@ -0,0 +1,13 @@
+﻿using Klasy;
+User user1 = new User("Adam", "32423ffddf");
+User user2 = new User("Monika", "32423ffddf");
+User user3 = new User("Zuzia", "32423ffddf");
+User user4 = new User("Damian", "32423ffddf");
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
+
+
+
