b2a8e2ac335c18d9bf2dc7da8a79538b19ea1f40
tak3maj <tak3maj@wp.pl>
2023-10-31 09:37:45 +0100

    Dzień 6.

---
+++ ChallengeApp/Emp.cs
@@ -0,0 +1,30 @@
+﻿namespace Employee
+{
+    public class Emp
+    {
+        private List<int> score = new List<int>();
+
+        public Emp(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Employee;
+//using System.Runtime.CompilerServices;
+//using System.Threading.Tasks.Sources;
+
+Emp emp1 = new Emp("Adam", "Grzyb");
+Emp emp2 = new Emp("Monika", "Woda");
+Emp emp3 = new Emp("Zuzia", "Trawa");
+
+emp1.AddScore(15);
+emp1.AddScore(2);
+emp1.AddScore(1);
+
+emp2.AddScore(2);
+emp2.AddScore(5);
+emp2.AddScore(-9);
+
+emp3.AddScore(11);
+emp3.AddScore(15);
+emp3.AddScore(1);
+
+var result = emp1.Result;
+var result1 = emp2.Result;
+var result2 = emp3.Result;
+
+List<Emp> emps = new List<Emp>()
+{
+    emp1, emp2, emp3
+};
+
+int maxResult = -1;
+Emp empWithMaxResult = null;
+
+foreach (var emp in emps)
+{
+    if (emp.Result > maxResult)
+    {
+        maxResult = emp.Result;
+        empWithMaxResult = emp;
+    }
+}
+Console.WriteLine($"Maximum punktów, czyli {maxResult} ma {empWithMaxResult.Name} {empWithMaxResult.Surname}");
