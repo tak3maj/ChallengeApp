cdbdc53222bd395eaa824f3447a078f5023f9815
tak3maj <tak3maj@wp.pl>
2023-11-04 12:19:51 +0100

    Dzień 9

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,41 +1,10 @@
-﻿using Employee;
-//using System.Runtime.CompilerServices;
-//using System.Threading.Tasks.Sources;
+﻿using ChallengeApp;
 
-Employee.Emp emp1 = new Employee.Emp("Adam", "Grzyb");
-Employee.Emp emp2 = new Employee.Emp("Monika", "Woda");
-Employee.Emp emp3 = new Employee.Emp("Zuzia", "Trawa");
-
-emp1.AddScore(15);
-emp1.AddScore(2);
-emp1.AddScore(1);
-
-emp2.AddScore(2);
-emp2.AddScore(5);
-emp2.AddScore(-9);
-
-emp3.AddScore(11);
-emp3.AddScore(15);
-emp3.AddScore(1);
-
-var result = emp1.Result;
-var result1 = emp2.Result;
-var result2 = emp3.Result;
-
-List<Employee.Emp> emps = new List<Employee.Emp>()
-{
-    emp1, emp2, emp3
-};
-
-int maxResult = -1;
-Employee.Emp empWithMaxResult = null;
-
-foreach (var emp in emps)
-{
-    if (emp.Result > maxResult)
-    {
-        maxResult = emp.Result;
-        empWithMaxResult = emp;
-    }
-}
-Console.WriteLine($"Maximum punktów, czyli {maxResult} ma {empWithMaxResult.Name} {empWithMaxResult.Surname}");
+var employee = new Employee("Adam", "Grzyb");
+employee.AddGrade(3);
+employee.AddGrade(51);
+employee.AddGrade(-8);
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
---
+++ Employee.Tests/Employee.Tests.cs
@@ -0,0 +1,57 @@
+using ChallengeApp;
+using System.Diagnostics;
+
+namespace Empl.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestValueOfMaximum()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Grzyb");
+            employee.AddGrade(20);
+            employee.AddGrade(2);
+            employee.AddGrade(8);
+
+            //act
+            var stat = employee.GetStatistics();
+            
+            //assert
+            Assert.AreEqual(20, stat.Max);
+        }
+
+        [Test]
+        public void TestValueOfMinimum()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Grzyb");
+            employee.AddGrade(20);
+            employee.AddGrade(2);
+            employee.AddGrade(8);
+
+            //act
+            var stat = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(2, stat.Min);
+        }
+
+        [Test]
+        public void TestValueOfAverage()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Grzyb");
+            employee.AddGrade(20);
+            employee.AddGrade(2);
+            employee.AddGrade(8);
+
+            //act
+            var stat = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(10, stat.Average);
+        }
+
+    }
+}
