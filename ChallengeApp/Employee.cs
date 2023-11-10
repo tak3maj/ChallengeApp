9a4f64613a34d459e0a3ede48a1b26f245480dfa
tak3maj <tak3maj@wp.pl>
2023-11-10 21:46:19 +0100

    Dzień 12

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -37,23 +37,23 @@ namespace ChallengeApp
             {
                 case 'A':
                 case 'a':
-                    this.grades.Add(100);
+                    this.AddGrade(100);
                     break;
                 case 'B':
                 case 'b':
-                    this.grades.Add(80);
+                    this.AddGrade(80);
                     break;
                 case 'C':
                 case 'c':   
-                    this.grades.Add(60);
+                    this.AddGrade(60);
                     break;
                 case 'D':
                 case 'd':   
-                    this.grades.Add(40);
+                    this.AddGrade(40);
                     break;
                 case 'E':
                 case 'e':
-                    this.grades.Add(20);
+                    this.AddGrade(20);
                     break;
                 default:
                     Console.WriteLine("Wrong Letter");
